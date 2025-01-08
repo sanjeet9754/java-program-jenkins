6994f0969e150d4518a0efa12da95a33ebe3558d
sanjeet <sanjeet.9754cool@gmail.com>
2025-01-08 21:57:56 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+class simpleprogram{
+    public static void main(String[] args) {
+        // Print some text
+        System.out.println("Hello, welcome to Java programming!");
+
+        // Get the current date
+        LocalDate currentDate = LocalDate.now();
+
+        // Print the current date
+        System.out.println("Today's date is: " + currentDate);
+    }
+}
